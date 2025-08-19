117c8d97cdf5e81c0bc8c7e151cc13d22ea6ce42
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-08-19 20:28:43 -0300

    refactor: reorganizes the XML documentation and improves the readability of the ServiceDto class

--- backend/Dtos/ServiceDto.cs
+++ backend/Dtos/ServiceDto.cs
@@ -1,11 +1,30 @@
 using System.ComponentModel.DataAnnotations;
 
+/// <summary>
+/// Represents a service model.
+/// </summary>
 public class ServiceDto
 {
+    /// <summary>
+    /// Gets or sets the name of the service.
+    /// </summary>
+    [MaxLength(50, ErrorMessage = "Name must be 50 characters or less")]
     public string? Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the duration of the service in minutes.
+    /// </summary>
+    [Range(1, int.MaxValue, ErrorMessage = "Duration must be greater than 0")]
     public int? DurationMinutes { get; set; }
+
+    /// <summary>
+    /// Gets or sets the description of the service.
+    /// </summary>
+    [MaxLength(200, ErrorMessage = "Description must be 200 characters or less")]
     public string? Description { get; set; }
 
-    [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
+    /// <summary>
+    /// Gets or sets the price of the service.
+    /// </summary>
     public decimal? Price { get; set; }
 }
