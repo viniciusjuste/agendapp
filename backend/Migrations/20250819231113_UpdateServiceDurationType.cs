1cfbbe286b9cd82874b37ed9d017c02e121a6ae0
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-08-19 20:26:43 -0300

    feat: atualiza o tipo da propriedade DurationMinutes para int na tabela Services

---
+++ backend/Migrations/20250819231113_UpdateServiceDurationType.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateServiceDurationType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DurationMinutes",
+                table: "Services",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DurationMinutes",
+                table: "Services",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
